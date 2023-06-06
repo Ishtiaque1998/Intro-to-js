6c0e0a935447493f31dfe70d5c76454152b94a59
Ishtiaque1997 <ishtiaque07031997@gmail.com>
2023-06-06 20:07:14 +0600

    lot more added

--- practice-js.js
+++ practice-js.js
@@ -1,3 +1,41 @@
-var str='89';
-var strNumber=parseInt(str);
-console.log(strNumber);
+//Problem-1
+
+var momGave=1000;
+console.log(momGave);
+
+var amountOfApple=1;
+var applePrice=120;
+var totalApplePrice=amountOfApple*applePrice;
+console.log(totalApplePrice);
+
+var amountOfOrange=1;
+var orangePrice=180;
+var totalOrangePrice=amountOfOrange*orangePrice
+console.log(totalOrangePrice);
+
+var returnAmount= momGave-totalApplePrice-totalOrangePrice;
+console.log(returnAmount);
+
+//Problem-2
+
+var mathMarks=75.25;
+var biologyMarks= 65;
+var chemistryMarks=80;
+var physicsMarks=35.45;
+var banglaMarks=99.50;
+
+var average= ((mathMarks+biologyMarks+chemistryMarks+physicsMarks+banglaMarks)/5);
+console.log(average.toFixed(2));
+
+//Problem-3
+
+var statement1= 'I am going to be';
+var statement2= 'an awesome web developer';
+
+console.log(statement1+ ' '+ statement2);
+
+//Problem-4
+
+var number1=119;
+var number2=5;
+console.log(number1%number2);
