524642398ca7a7e6d2bbb3c62c88f7787c3169c2
Ishtiaque1997 <ishtiaque07031997@gmail.com>
2023-06-06 12:47:38 +0600

    some more added

--- shorthand.js
+++ shorthand.js
@@ -7,4 +7,22 @@ tomatoPrice=56;
 
 //add something to the previous one
 tomatoPrice=tomatoPrice+20;
-console.log(tomatoPrice);
+//console.log(tomatoPrice);
+
+price1=39;
+
+
+// price1+=20; [add 20 to the existing value]
+// price1-=20; [subtract 20 to the existing value]
+// price1*=10; [multiply 10 to the existing value] 
+price1/=5;
+// console.log(price1);
+
+
+var age=40;
+//add 1 
+// age++;
+//subtract 1 from existing value
+age--;
+console.log(age)
+
